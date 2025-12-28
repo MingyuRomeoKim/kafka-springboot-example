d802e21ff011604cc0fec83c6c3c464ea18efc9c
mingyu <mingyu@mingyuui-MacBookPro.local>
2025-12-28 12:19:52 +0900

    #init, 초기 spring boot 2.7 , Jdk8

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kafka Demo Project
+본 프로젝트는 nice infra Edge Client 프로그램의 차세대 준비를 실행하기 위한 테스트 파일럿 프로젝트로, 아래 버전을 주로 사용합니다.
+ 
+## Version
+- JDK 1.8
+- Kafka 3.5.2 
+- Gradle 7.2
---
+++ src/test/java/kr/co/kimmingyu/kafka_demo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kimmingyu.kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
